5debbeb0c7041be58a07b468a915d74a4e3030a7
Bikashkumar <115339734+bikash192@users.noreply.github.com>
2025-02-10 20:20:54 +0530

    Time: 4 ms (43.33%) | Memory: 43.5 MB (35.74%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,27 +1,29 @@
 class Solution {
-    public void c_sum(int[]a,int id,int sum,int t,List<Integer> cur,List<List<Integer>> ans){
-            if(sum==t){
-                ans.add(new ArrayList<>(cur));
-                return;
-            }
-        if (sum > t || id == a.length) return;
-        sum+=a[id];
+    public void c_sum(int[] a, int id, int sum, int t, List<Integer> cur, List<List<Integer>> ans) {
+        if (sum == t) {
+            ans.add(new ArrayList<>(cur));
+            return;
+        }
+        if (sum > t || id == a.length)
+            return;
+        sum += a[id];
         cur.add(a[id]);
-        c_sum(a,id+1,sum,t,cur,ans);
-        sum-=a[id];
-        cur.remove(cur.size()-1);
-        int idx=id+1;
-        while(idx<a.length&&a[idx-1]==a[idx]){
+        c_sum(a, id + 1, sum, t, cur, ans);
+        sum -= a[id];
+        cur.remove(cur.size() - 1);
+        int idx = id + 1;
+        while (idx < a.length && a[idx - 1] == a[idx]) {
             idx++;
         }
-        c_sum(a,idx,sum,t,cur,ans);
+        c_sum(a, idx, sum, t, cur, ans);
 
     }
+
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> cur=new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> cur = new ArrayList<>();
         Arrays.sort(candidates);
-        c_sum(candidates,0,0,target,cur,ans);
+        c_sum(candidates, 0, 0, target, cur, ans);
         return ans;
     }
 }
