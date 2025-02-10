3ff512a3ab3162d7e39c39ef3b5cf818fa2306ca
Bikashkumar <115339734+bikash192@users.noreply.github.com>
2025-02-10 20:17:21 +0530

    Time: 5 ms (20.37%) | Memory: 43.4 MB (35.74%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,23 +1,27 @@
 class Solution {
-    public void c_sum(int []a,int id,int t,List<Integer> cur,List<List<Integer>> ans){
-        if(t==0){
-            ans.add(new ArrayList<>(cur));
-            return;
+    public void c_sum(int[]a,int id,int sum,int t,List<Integer> cur,List<List<Integer>> ans){
+            if(sum==t){
+                ans.add(new ArrayList<>(cur));
+                return;
+            }
+        if (sum > t || id == a.length) return;
+        sum+=a[id];
+        cur.add(a[id]);
+        c_sum(a,id+1,sum,t,cur,ans);
+        sum-=a[id];
+        cur.remove(cur.size()-1);
+        int idx=id+1;
+        while(idx<a.length&&a[idx-1]==a[idx]){
+            idx++;
         }
-        for(int i=id;i<a.length;i++){
-            if(i>id&&a[i]==a[i-1]) continue;
-            if(a[id]>t) break;
-            cur.add(a[i]);
-            c_sum(a,i+1,t-a[i],cur,ans);
-            cur.remove(cur.size()-1);
-        }
+        c_sum(a,idx,sum,t,cur,ans);
 
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> ans=new ArrayList<>();
         List<Integer> cur=new ArrayList<>();
         Arrays.sort(candidates);
-        c_sum(candidates,0,target,cur,ans);
+        c_sum(candidates,0,0,target,cur,ans);
         return ans;
     }
 }
