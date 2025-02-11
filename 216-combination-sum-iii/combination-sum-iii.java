7cb752db8ffa684633ae8e03092dacfe53cec155
Bikashkumar <115339734+bikash192@users.noreply.github.com>
2025-02-11 15:24:24 +0530

    Time: 1 ms (20.50%) | Memory: 40.9 MB (77.49%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -1,24 +1,22 @@
 class Solution {
-    public void c_sum(int i,int sum,int k,int n,List<Integer>cur,           List<List<Integer>> ans){
-        if(sum>n) return ;
-        if(k==0){
-            if(sum==n){
+    public void c_sum(int i,int sum,int k,int n,List<Integer> cur,List<List<Integer>> ans){       if(sum>n) return ;
+        if(sum==n){
+            if(k==0){
                 ans.add(new ArrayList<>(cur));
             }
             return ;
         }
-         if(i==10) return;
-        cur.add(i);
+        if(i==10) return ;
         sum+=i;
+        cur.add(i);
         c_sum(i+1,sum,k-1,n,cur,ans);
-        cur.remove(cur.size()-1);
         sum-=i;
+        cur.remove(cur.size()-1);
         c_sum(i+1,sum,k,n,cur,ans);
-
     }
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> cur=new ArrayList<>();
+        List<Integer>cur=new ArrayList<>();
         c_sum(1,0,k,n,cur,ans);
         return ans;
     }
