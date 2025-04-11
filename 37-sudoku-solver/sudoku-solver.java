54581567060959b347bb38136ae66ee491edbd6e
Bikashkumar <115339734+bikash192@users.noreply.github.com>
2025-04-11 15:11:00 +0530

    Time: 221 ms (53.63%) | Memory: 41 MB (86.08%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,44 +1,57 @@
 class Solution {
-    static int check=0;
-    public boolean isValid(char[][]board,int row,int col,char num){
-        for(int j=0;j<9;j++){
-            if(board[row][j]==num) return false;
+    public void solve(int row, int col, char[][] board, char[][] ans) {
+        if (col == 9) {
+            row++;
+            col = 0;
         }
-        for(int i=0;i<9;i++){
-            if(board[i][col]==num) return false;
-        }
-        int srow=row/3*3;
-        int scol=col/3*3;
-        for(int i=srow;i<srow+3;i++){
-            for(int j=scol;j<scol+3;j++){
-                if(board[i][j]==num) return false;
+        if (col == 0 && row == 9) {
+            // Manually copy board to ans
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    ans[i][j] = board[i][j];
+                }
             }
+            return;
         }
-        return true;
-    }
-    public void solve(char[][]board,int row,int col){
-        if(row==9){
-            check=1;
-            return ;
-        }
-        else if(board[row][col]!='.'){
-            if(col!=8) solve(board,row,col+1);
-            else solve(board,row+1,0);
-        }
-        else{
-            for(char ch='1';ch<='9';ch++){
-                if(isValid(board,row,col,ch)){
-                    board[row][col]=ch;
-                    if(col!=8) solve(board,row,col+1);
-                    else solve(board,row+1,0);
-                    if(check==1) return;
-                    board[row][col]='.';
+        if (board[row][col] != '.') {
+            solve(row, col + 1, board, ans);
+        } else {
+            for (char ch = '1'; ch <= '9'; ch++) {
+                if (isValid(board, row, col, ch)) {
+                    board[row][col] = ch;
+                    solve(row, col + 1, board, ans);
+                    board[row][col] = '.'; // backtrack
                 }
             }
         }
     }
+
+    public boolean isValid(char[][] board, int row, int col, char ch) {
+        for (int j = 0; j < 9; j++) {
+            if (board[row][j] == ch) return false;
+        }
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == ch) return false;
+        }
+        int srow = row / 3 * 3;
+        int scol = col / 3 * 3;
+        for (int i = srow; i < srow + 3; i++) {
+            for (int j = scol; j < scol + 3; j++) {
+                if (board[i][j] == ch) return false;
+            }
+        }
+        return true;
+    }
+
     public void solveSudoku(char[][] board) {
-      solve(board,0,0); 
-      check=0; 
+        char[][] ans = new char[9][9];
+        solve(0, 0, board, ans);
+
+        // Copy ans back into board (so changes reflect in the original board)
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = ans[i][j];
+            }
+        }
     }
 }
